57f92dbdcd6be93feb114825e8b68a88a71581dd
Ruben Contreras <SteelMods@hotmail.com>
2023-01-30 19:57:13 -0500

    Mini-project logic ended

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import Characters from "./components/Characters";
+import Navbar from "./components/Navbar";
+import Pagination from "./components/Pagination";
+
+function App() {
+  const [characters, setCharacters] = useState();
+  const [info, setInfo] = useState({});
+
+  const API = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (urlAPI) => {
+    fetch(urlAPI)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  };
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  };
+
+  useEffect(() => {
+    fetchCharacters(API);
+  }, []);
+
+  return (
+    <>
+      <Navbar brand={"Rick and Morty App"} />
+
+      <div className="container mt-5">
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+        <Characters characters={characters} />
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
